d74b4c2483374307cfba09f01107a112f4ef1c38
fjohnette <fjohnette@pm.me>
2024-08-22 13:43:34 -0500

    skip articles missing information

--- app/services/rss_feed_parser.rb
+++ app/services/rss_feed_parser.rb
@@ -28,43 +28,56 @@ class RssFeedParser
   end
 
   def extract_articles
-  return if @feed.errors.any?
+    return if @feed.errors.any?
   
-  articles = []
-  atom_ns = { 'atom' => 'http://www.w3.org/2005/Atom' }
+    articles = []
+  
+    # Define namespaces
+    namespaces = {
+      'atom' => 'http://www.w3.org/2005/Atom',
+      'media' => 'http://search.yahoo.com/mrss/',
+      'dc' => 'http://purl.org/dc/elements/1.1/'
+    }
 
-  # Check if the document is an RSS feed
-  if @doc.at_xpath('//rss/channel/item')
-    # RSS Feed Parsing
-    @doc.xpath('//rss/channel/item').each do |item|
-      article = {
-        title: item.at_xpath('title')&.text,
-        link: item.at_xpath('link')&.text,
-        description: item.at_xpath('description')&.text,
-        pub_date: item.at_xpath('pubDate')&.text,
-        image: item.at_xpath('image')&.text, # If there's an <image> tag within <item>
-        audio_link: item.at_xpath('enclosure[@type="audio/mpeg"]')&.[]('url') # Extract audio link if present
-      }
+    # Check if the document is an RSS feed
+    if @doc.at_xpath('//rss/channel/item')
+      # RSS Feed Parsing
+      @doc.xpath('//rss/channel/item').each do |item|
+        article = {
+          title: item.at_xpath('title')&.text,
+          link: item.at_xpath('link')&.text,
+          description: item.at_xpath('description')&.text,
+          pub_date: item.at_xpath('pubDate')&.text,
+          image: item.at_xpath('media:thumbnail/@url', namespaces)&.text || item.at_xpath('image')&.text,
+          audio_link: item.at_xpath('enclosure[@type="audio/mpeg"]/@url')&.text # Extract audio link if present
+        }
+  
+      # If description is not found, attempt to retrieve it from 'media:description'
+      article[:description] ||= item.at_xpath('media:description', namespaces)&.text
 
+      # If pubDate is not found, attempt to retrieve it from 'dc:date'
+      article[:pub_date] ||= item.at_xpath('dc:date', namespaces)&.text
+      
+      next if article[:pub_date].nil? or article[:description].nil?
       articles << article
     end
   
-  # Check if it's an Atom feed
-  elsif @doc.at_xpath('//atom:feed/atom:entry', atom_ns)
-    # Atom Feed Parsing
-    @doc.xpath('//atom:feed/atom:entry', atom_ns).each do |entry|
-      article = {
-        title: entry.at_xpath('atom:title', atom_ns)&.text,
-        link: entry.at_xpath('atom:link[@rel="alternate"]', atom_ns)&.[]('href'),
-        description: entry.at_xpath('atom:content', atom_ns)&.text || entry.at_xpath('atom:summary', atom_ns)&.text,
-        pub_date: entry.at_xpath('atom:published', atom_ns)&.text || entry.at_xpath('atom:updated', atom_ns)&.text,
-        image: entry.at_xpath('atom:content//img/@src', atom_ns)&.text # Extract the first image src if present
-      }
-
-      articles << article
+    # Check if it's an Atom feed
+    elsif @doc.at_xpath('//atom:feed/atom:entry', namespaces)
+      # Atom Feed Parsing
+      @doc.xpath('//atom:feed/atom:entry', namespaces).each do |entry|
+        article = {
+          title: entry.at_xpath('atom:title', namespaces)&.text,
+          link: entry.at_xpath('atom:link[@rel="alternate"]', namespaces)&.[]('href'),
+          description: entry.at_xpath('atom:content', namespaces)&.text || entry.at_xpath('atom:summary', namespaces)&.text,
+          pub_date: entry.at_xpath('atom:published', namespaces)&.text || entry.at_xpath('atom:updated', namespaces)&.text,
+          image: entry.at_xpath('atom:content//img/@src', namespaces)&.text # Extract the first image src if present
+        }
+  
+        articles << article
+      end
     end
+  
+    articles
   end
-
-  articles
-end
 end
